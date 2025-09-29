6e970a1d260b8ae2048c6264b34ea1e993a1118d
gabrieldemarco1209 <gabriel.demarco2007@gmail.com>
2025-09-29 16:48:37 -0300

    complementando tela home criando a tela de cadastro do restaurante.

--- lib/tela_home.dart
+++ lib/tela_home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mimpedir/tela_cad_restaurante.dart';
 
 
 class TelaHome extends StatelessWidget {
@@ -10,9 +11,34 @@ class TelaHome extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Tela home")),
-      body: Center(
-        child: Text('Seja bem vindo ao Mim Pedir!!!:3'),
-      ),
+      body: Padding(padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Lista de Restaurantes:  "),
+          ElevatedButton(onPressed: (){  Navigator.push(context, MaterialPageRoute(builder: (context) => TelaCadRestaurante())
+            );
+          }, child:  Text("Cadastrar Restaurantes")),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:
+            [
+              Column(
+            children: [
+                Text("Meu restaurante"),
+                Text("Comida Mexicana")
+                ],
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(onPressed: (){}, child: Text("Excluir",)),
+                SizedBox(width: 10),
+                ElevatedButton(onPressed: (){}, child: Text("Excluir",)),
+              ],
+            ),
+          ],
+        ),
+      )
     );
   }
 }
