cbc7c1e300a6d5f39eeffa6f2ce74dfb7f2227b2
Gabrieldemarco1209 <gabriel.demarco2007@gmail.com>
2025-10-20 17:22:08 -0300

    Refactor TelaCadRestaurante to StatefulWidget

--- lib/tela_cad_restaurante.dart
+++ lib/tela_cad_restaurante.dart
@@ -1,7 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:mimpedir/tipo.dart';
+import 'banco/restaurante_DAO.dart';
+import 'banco/tipo_DAO.dart';
 
-class TelaCadRestaurante extends StatelessWidget {
-  TelaCadRestaurante({super.key});
+class TelaCadRestaurante extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return TelaCadRestauranteState();
+  }
+  //TelaCadRestaurante({super.key});
+}
+
+class TelaCadRestauranteState extends State<TelaCadRestaurante> {
+
+  final TextEditingController nomeController = TextEditingController();
+  final TextEditingController latitudeController = TextEditingController();
+  final TextEditingController longitudeController = TextEditingController();
+  String? culinariaSelecionada;
+  List<Tipo> tiposCulinaria =[];
+  int? tipoCulinaria;
+
+  void initState(){
+    super.initState();
+    carregarTipos();
+  }
+
+  Future<void> carregarTipos() async{
+    final lista = await TipoDAO.listarTipos();
+    setState(() {
+      tiposCulinaria = lista;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,40 +38,77 @@ class TelaCadRestaurante extends StatelessWidget {
       appBar: AppBar(title: const Text("Cadastro de Restaurante")),
       body: Padding(padding: const EdgeInsets.all(30),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Informações do Restaurante: "),
-            SizedBox(height: 40),
-            Text("tipo de comida: "),
-            DropdownButtonFormField(
-                items: [
-                  DropdownMenuItem(value: "Japonesa", child: Text("Japonesa")),
-                  DropdownMenuItem(value: "Italiana", child: Text("Italiana")),
-                  DropdownMenuItem(value: "Brasileira", child: Text("Brasileira")),
-                  ],
-              onChanged: (value){}),
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Informações do Restaurante: "),
+              SizedBox(height: 40),
+              Text("Tipo de comida: "),
+              DropdownButtonFormField(
+
+                  value: culinariaSelecionada,
+                  items: tiposCulinaria.map((tipo){
+                    return DropdownMenuItem<String>(
+                        value: tipo.nome,
+                        child: Text("${tipo.nome}"),
+                    );
+                  }).toList(),
+                  onChanged: (String? value){
+                    setState((){
+                      culinariaSelecionada = value;
+                      Tipo tipoSelecionado = tiposCulinaria.firstWhere(
+                          (tipo) => tipo.nome == value,
+                      );
+                     tipoCulinaria = tipoSelecionado.codigo;//vamos ver
+                    });
+                  }
+                ),
               TextFormField(
-                decoration: const InputDecoration(hintText: 'Nome do Restaurante'),
+                decoration: const InputDecoration(
+                    hintText: 'Nome do Restaurante'),
                 validator: (String? value) {},
+                controller: nomeController,
               ),
               TextFormField(
                 decoration: const InputDecoration(hintText: 'Latitude'),
                 validator: (String? value) {},
+                controller: latitudeController,
               ),
-        TextFormField(
-          decoration: const InputDecoration(hintText: 'Longitude'),
-          validator: (String? value) {},
+              TextFormField(
+                decoration: const InputDecoration(hintText: 'Longitude'),
+                validator: (String? value) {},
+                controller: longitudeController,
+              ),
+              SizedBox(height: 50),
+              ElevatedButton(
+                  onPressed: () async{
+                    final sucesso = await RestauranteDAO.cadRestaurante(
+                        nomeController.text, latitudeController.text, longitudeController.text, tipoCulinaria);
+                    String msg = 'Erro: não cadastrado. Verifique os dados.';
+                    Color corFundo = Colors.red;
+
+                    if(sucesso > 0){
+                        msg = '"${nomeController.text}" cadastrado com sucesso! ID: $sucesso';
+                        Color corFundo = Colors.green;
+                    }
+                    ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(msg),
+                      backgroundColor: Colors.green,
+                      duration: Duration(seconds: 5),
+                      )
+                    );
+                  },
+                  child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.save),
+                  Text("Cadastrar")
+                ],
+              )
+              )
+            ]
         ),
-            ElevatedButton(onPressed: (){}, child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.save),
-                Text("Cadastrar")
-          ],
-        ))
-      ]
-        ),
-      ),//...
+      ),
     );
   }
 }
